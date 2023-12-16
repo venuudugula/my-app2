d7907656e4080f290f0429199057e5368e66ee15
venuudugula <uvenu3349@gmail.com>
2023-12-16 12:08:53 +0530

    pre load implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
@@ -72,7 +72,11 @@ const routes: Routes = [
     {path:'rating',component:RatingComponent},
     {path:'rating-child',component:RatingChildComponent},
     {path:'text-area',component:TextAreaComponent},
-    {path:'about-company',component:AboutCompanyComponent}
+    {path:'about-company',component:AboutCompanyComponent},
+    {
+      path: 'payments',
+      loadChildren: () => import('./payments/payments.module').then(m => m.PaymentsModule)
+    }
     
 
     ]},
@@ -84,7 +88,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
